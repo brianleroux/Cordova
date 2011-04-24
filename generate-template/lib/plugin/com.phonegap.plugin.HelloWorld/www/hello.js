54d9ccccf6f2e743a7dd4a3abdb50b816aaea612
brianleroux <brian@westcoastlogic.com>
2011-04-24 00:51:54 -0700

    whoops, accidental plugin add

--- generate-template/lib/plugin/com.phonegap.plugin.HelloWorld/www/hello.js
+++
@@ -1,8 +0,0 @@
-var hello = function(name, win, fail) {
-   PhoneGap.exec(win, fail, "HelloWorld", "hello", [name]);
-}
-
-// this is only for Android..for now
-PhoneGap.addConstructor(function() {
-   navigator.app.addService('HelloWorld','com.phonegap.plugin.HelloWorld');
-})
