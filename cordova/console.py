3ed5c18a9b2eeb7ef83adb3a8b82be9347125674
Bernardo Heynemann <heynemann@gmail.com>
2011-05-13 15:16:59 -0300

    Console vows

--- cordova/console.py
+++ cordova/console.py
@@ -9,10 +9,21 @@ class Console(object):
     def __init__(self):
         arguments = sys.argv
         self.executable = arguments[0]
-        self.command = arguments[1]
+        if len(arguments) < 2:
+            self.command = 'help'
+        else:
+            self.command = arguments[1]
+
+        self.arguments = []
+        if (len(arguments) > 2):
+            self.arguments = arguments[2:]
 
     def run(self):
-        COMMANDS[self.command](self).run()
+        command = [command for command in COMMANDS if command.key == self.command]
+        if command:
+            command[0](self).run()
+        else:
+            HelpCommand(self).run()
 
 class Command(object):
 
@@ -22,14 +33,51 @@ class Command(object):
     def run(self):
         raise NotImplementedError()
 
+def print_version():
+    print 'PhoneGap - Cordova v%d.%d.%d' % __version__
+
 class VersionCommand(Command):
+    key = 'version'
 
     def run(self):
-        print 'PhoneGap - Cordova v%d.%d.%d' % __version__
+        print_version()
 
-COMMANDS = {
-    'version': VersionCommand
-}
+    @staticmethod
+    def print_overview():
+        print '    version - show the currently installed version of Cordova'
+
+class HelpCommand(Command):
+    key = 'help'
+
+    def run(self):
+        self.print_usage()
+        print
+
+        if not self.console.arguments:
+            self.print_commands()
+
+            print
+            print "See 'phonegap help <command>' for more information on a specific command."
+            print
+
+            print_version()
+
+    def print_usage(self):
+        print 'Usage: phonegap command [options]'
+
+    @staticmethod
+    def print_overview():
+        print '    help - show this help message and exit'
+
+    def print_commands(self):
+        print "Available commands:"
+        for command in COMMANDS:
+            command.print_overview()
+
+COMMANDS = [
+    VersionCommand,
+    HelpCommand
+]
 
 if __name__ == '__main__':
     console = Console()
--- tests/console_vows.py
+++ tests/console_vows.py
@@ -23,8 +23,24 @@ class ConsoleApp(Vows.Context):
         def should_be_equal_to_version(self, topic):
             expect(topic).to_be_like('PhoneGap - Cordova v%d.%d.%d' % __version__)
 
-    #class HelpCommand(Vows.NotErrorContext, Vows.NotEmptyContext):
+    class HelpCommand(Vows.NotErrorContext, Vows.NotEmptyContext):
 
-        #def topic(self):
-            #return execute('help')
+        def topic(self):
+            return execute('help')
+
+        def should_include_version(self, topic):
+            expect(topic).to_include('PhoneGap - Cordova v%d.%d.%d' % __version__)
+
+        def should_include_usage(self, topic):
+            expect(topic).to_include('Usage: phonegap command [options]')
+
+        def should_include_list_of_commands(self, topic):
+            expect(topic).to_include('Available commands:')
+
+        def should_include_version_command(self, topic):
+            expect(topic).to_include('version - show the currently installed version of Cordova')
+
+        def should_include_help_command(self, topic):
+            expect(topic).to_include('    help - show this help message and exit')
+
 
