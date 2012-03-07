a2dd7139c3d3b091cdc380aa64b4a6be04a9ff1f
Brian LeRoux <b@brian.io>
2012-03-07 14:22:44 -0800

    removed everything

--- cordova/recipes/big3/js/app.js
+++
@@ -1,8 +0,0 @@
-function onLoad() {
-	x$(document.body).setStyle('height', screen.height + 'px');
-	document.addEventListener("deviceready", onDeviceReady, false);
-}
-
-function onDeviceReady() {
-	x$(document.body).inner('onDeviceReady()');
-}
